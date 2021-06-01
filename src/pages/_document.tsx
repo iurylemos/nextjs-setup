c707ae2084e9a4ba2b3061bfa51ea0674595c4f9
Claudio Iury <iurylemos4@gmail.com>
2021-06-01 00:59:10 -0300

    init setup with ts and styled-components

---
+++ src/pages/_document.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Document, { DocumentContext, DocumentInitialProps, Html, NextScript, Main, Head } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                })
+
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            }
+        } finally {
+            sheet.seal()
+        }
+    }
+
+    render() {
+        return (
+            <Html lang='pt'>
+                <Head>
+                    <meta charSet="utf-8" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;1,100;1,300&display=swap" rel="stylesheet" />
+                    <link rel="icon" href="https://rocketseat.com.br/favicon.ico" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
